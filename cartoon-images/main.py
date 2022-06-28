e36e98b93251cc42510075803d7090a0cea83d2a
Yona Hirakawa <hirakawayona@gmail.com>
2022-06-28 15:59:22 -0300

    imagem em cartoon

---
+++ cartoon-images/main.py
@@ -0,0 +1,24 @@
+# Importing libraries
+import cv2
+
+image = "grumpy-cat.jpg"
+
+# Reading image
+img = cv2.imread(image)
+
+# Edges
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+edges = cv2.adaptiveThreshold(gray, 255,
+        cv2.ADAPTIVE_THRESH_MEAN_C,
+        cv2.THRESH_BINARY, 9, 9)
+
+# Cartoonization
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask = edges)
+cv2.imshow("Image", img)
+cv2.imshow("edges", edges)
+cv2.imshow("Cartoon", cartoon)
+cv2.imwrite(image.split()[0]+"_Cartoon.png",cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
